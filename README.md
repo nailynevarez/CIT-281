821763799565f214b86a7eb49b04079988f56eb9
nailyn <nailyn@uoregon.edu>
2016-04-14 16:13:18 -0700

    Initial commit on p2 branch

---
+++ README.md
@@ -0,0 +1,4 @@
+# CIT 281 Project 2 Branch
+## Github repo for Project 2
+***
+[Hello-World repo](https://github.com/nailynevarez/hello-world)
---
+++ javascripts/functions.js
@@ -0,0 +1,57 @@
+function arrAvg([numbers]) {
+    var sum = 0;
+    var i = 0;
+    var average;
+
+    for (i; i < numbers.length; i++) {
+        sum = sum + numbers[i];
+        average = (sum / numbers.length);
+    };
+    return average;
+};
+
+function arrMax(numbers) {
+    var max;
+    var i = 0;
+    for (i; i < numbers.length; i++) {
+        if (numbers[i] > numbers.length - 1) {
+            max = numbers[i];
+        }
+    };
+    return max;
+};
+
+function isMember(arr, Strng) {
+var i;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === Strng) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+
+var hasOneEven = function(arr) {
+var i;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i]/2 = 0) {
+            return true
+        }
+    }
+};
+
+function maxWord(sentence) {
+    var longestString = 0;
+    var string = sentence.split(" ");
+    var word = 0;
+    for (var i = 0; i < string.length; i++) {
+        if (longestString < string[i].length) {
+            longestString = string[i].length;
+            word = string[i];
+        }
+    }
+    return word.length;
+}   
