4228a7b313a280df0ae98692c6772f6c4268aa17
nailyn <nailyn@uoregon.edu>
2016-04-15 12:10:21 -0700

    Modified functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,4 +1,5 @@
 function arrAvg([numbers]) {
+    "use strict";
     var sum = 0;
     var i = 0;
     var average;
@@ -11,6 +12,7 @@ function arrAvg([numbers]) {
 };
 
 function arrMax(numbers) {
+    "use strict";
     var max;
     var i = 0;
     for (i; i < numbers.length; i++) {
@@ -22,6 +24,7 @@ function arrMax(numbers) {
 };
 
 function isMember(arr, Strng) {
+    "use strict";
     var i;
     for (i = 0; i < arr.length; i++) {
         if (arr[i] === Strng) {
@@ -32,16 +35,46 @@ function isMember(arr, Strng) {
     return false;
 }
 
+/*hasOneEven returns true if arr contains at least one even number*/
 var hasOneEven = function(arr) {
+    "use strict";
     var i;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] / 2 = 0) {
-            return true
-        }
+    //search for even number in arr
+    for (i = 0; i < arr.length; ++i) {
+        if (arr[i] % 2 === 0)
+        //hit
+            return true;
     }
+    //miss
+    return false;
 };
 
+var a = [3, 5, 7, 2];
+var sum = 0;
+a.forEach(function(val) {
+    sum = sum + val; 
+});
+
+var a = ["a", "b", "c", "d", "a"];
+var isMember = function(arr, s) {
+    timesSoFar = 0;
+    arr.forEach(function(value) {
+        if(value === s){
+            //hit
+        timesSoFar = timesSoFar + 1;
+    }
+    });
+    //return result
+    if (timesSoFar >= 2)
+    return true;
+    else 
+    return false;
+};
+
+
+
 function maxWord(sentence) {
+    "use strict";
     var longestString = 0;
     var string = sentence.split(" ");
     var word = 0;
